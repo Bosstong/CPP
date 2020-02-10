86a346ece59cfdd27751e58ce3fda606713cdf28
Bosstong <240136159@qq.com>
2020-02-09 22:33:49 -0800

    added exe10.cpp file

---
+++ exe10.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+main(){
+    int a=4,b=6,c=8,res1,res2;
+    res1=a,res2=b+c;
+    for(int i =0;i<2;i++)
+    {
+        cout<<res1<<' '<<res2<<endl;
+    }
+}
