e15cd2ac973f37fdc8fba49357c32fb78eeccdf9
Bosstong <240136159@qq.com>
2020-02-09 22:49:16 -0800

    added exe12.cpp file

---
+++ exe12.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+main(){
+    int ilnput;
+    cout<<"input a value:"<<endl;
+    cin>>ilnput;
+    if(ilnput%2!=0)
+    {
+    cout<<"The value is odd number"<<endl;
+    }else
+    {
+     cout<<"The value is even number"<<endl;
+    }
+    
+}
