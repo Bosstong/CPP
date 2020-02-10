a2b79aba75491fc81ffb383028d49e9bb00f7801
Bosstong <240136159@qq.com>
2020-02-09 22:33:31 -0800

    added exe9.cpp file

---
+++ exe9.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+main(){
+    int a,b,c;
+    a=sizeof(char);
+    b=sizeof(void*);
+    c=sizeof(66);
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+}
