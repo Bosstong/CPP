4520250b2dfda3f3e74d417bfb1c158c83507587
Bosstong <240136159@qq.com>
2020-02-09 23:56:41 -0800

    added exe14.cpp file

---
+++ exe14.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+main(){
+    int ilnput;
+    cout<<"input number"<<endl;
+    cin>>ilnput;
+    (ilnput%3==0)?
+    ((ilnput%5==0)?cout<<"yes":cout<<"no")
+    :cout<<"no";
+    cout<<endl;
+}
