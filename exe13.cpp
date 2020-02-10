9f9e5a11198b0fbd98eab026ac99fa7073e69f55
Bosstong <240136159@qq.com>
2020-02-09 23:56:29 -0800

    added exe13.cpp file

---
+++ exe13.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+main(){
+    int ilnput;
+    cin>>ilnput;
+    if(ilnput>90)
+    cout<<"It is Good"<<endl;
+    else
+    {
+        cout<<"It is not Good"<<endl;
+    }
+    
+}
